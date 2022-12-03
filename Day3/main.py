0ee0157e968e3fd903ffd8119d6b80c79e28b382
Ukhutula <114575481+Ukhutula@users.noreply.github.com>
2022-12-04 08:01:49 +1100

    Add function to check for duplicates

--- Day3/main.py
+++ Day3/main.py
@@ -1,11 +1,10 @@
-
+from string import ascii_lowercase, ascii_uppercase
 
 def readandstrip(filename):
     file = open(filename, 'r')
     lines = file.readlines()
     strippedlines = [line.strip('\n') for line in lines]
     return strippedlines
-
 
 def splitintwo(line):
     length = len(line)
@@ -14,11 +13,18 @@ def splitintwo(line):
         return splitline
     else:
         print("Odd number of characters on line " + line)
-    
+
+def find_duplicates(line):
+    duplicates = [letter for letter in line[0] if letter in line[1]]
+    return set(duplicates)
 
 
 if __name__ == "__main__":
     lines = readandstrip('Day3\input.txt')
     for line in lines:
         splitline = splitintwo(line)
-        print(splitline)
+        duplicates = find_duplicates(splitline)
+
+alphabet = ascii_lowercase + ascii_uppercase
+priorities = {key:value for (value, key) in enumerate(alphabet, 1)}
+print(priorities)
