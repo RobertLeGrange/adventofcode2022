610ac336145dcb755e16bdce8c1767db73bcf873
Ukhutula <114575481+Ukhutula@users.noreply.github.com>
2022-12-04 07:17:21 +1100

    Add function to split lines in half

--- Day3/main.py
+++ Day3/main.py
@@ -6,6 +6,19 @@ def readandstrip(filename):
     strippedlines = [line.strip('\n') for line in lines]
     return strippedlines
 
+
+def splitintwo(line):
+    length = len(line)
+    if length % 2 == 0:
+        splitline = [line[:length//2], line[length//2:]]
+        return splitline
+    else:
+        print("Odd number of characters on line " + line)
+    
+
+
 if __name__ == "__main__":
     lines = readandstrip('Day3\input.txt')
-    print(lines)
+    for line in lines:
+        splitline = splitintwo(line)
+        print(splitline)
