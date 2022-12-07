db4bdd701835f809140d7a4e7372795e9950e290
Ukhutula <114575481+Ukhutula@users.noreply.github.com>
2022-12-07 21:34:41 +1100

    Add CrateMover 9000

--- Day5/main.py
+++ Day5/main.py
@@ -1,18 +1,32 @@
-data = open("Day5\input.txt").read()
+import re
+
+data = open("Day5\input.txt").read().rstrip()
 lines = data.split("\n")
 
-positions = [lines[8].index(str) for str in lines[8] if str.isdigit()]
+stacklines, positionlines, commandlines = lines[:8], lines[8], lines[10:]
 
+positions = [positionlines.index(str) for str in positionlines if str.isdigit()]
 
 stacks = []
 stack = []
 for position in positions:
-    for line in reversed(lines[:9]):
+    for line in reversed(stacklines):
         for str in line[position]:
             if str.isalpha():
                 stack.append(str)
     stacks.append(stack)
     stack = []
 
+pattern = re.compile("move | from | to ")
 
-print(stacks)
+#CrateMover 9000
+for line in commandlines:
+    numcrate, fromcrate, tocrate = list(map(int, re.split(pattern, line)[1:]))
+    for i in range(numcrate):
+        tempcrate = stacks[fromcrate - 1].pop()
+        stacks[tocrate - 1].append(tempcrate)
+
+topstack = ''
+for stack in stacks:
+    topstack += stack[-1]
+print(topstack)
